50a6f0dfa27f265777c119e26a2aa36572e73911
anruiqi <59909691+anruiqi@users.noreply.github.com>
2020-02-29 15:09:24 +0800

    安装ElementUI

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from './element-ui' // 引入UI
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App)
