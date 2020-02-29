a78a5af2a203c9b787918a51244cffb88d3a319b
anruiqi <59909691+anruiqi@users.noreply.github.com>
2020-02-29 20:14:05 +0800

    新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/home' // 用一种简写的方式，代替
+import Login from '@/views/login'
 
 Vue.use(VueRouter)
 
@@ -13,6 +14,12 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/login',
+    component: Login,
+    name: 'login'
+
   }
   // {
   //   // 按需加载
---
+++ src/views/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <span>登录页</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
