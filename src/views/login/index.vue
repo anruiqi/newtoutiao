b0356d0bbf844ccc5c82500b44be10a0d22a9603
anruiqi <59909691+anruiqi@users.noreply.github.com>
2020-03-02 16:37:07 +0800

    修改登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -41,13 +41,22 @@ export default {
 
 <style lang='less' scoped>
 .login {
-  background-image: url(../../assets/img/beijing.jpg);
   height: 100vh; // 当前屏幕可视区域分成一百份
-  background-size: cover;
   display: flex;
   justify-content: center;
   align-items: center;
+  &:before {
+    content: '132';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-image: url(../../assets/img/beijing.jpg);
+    filter: blur(2px);
+    background-size: cover;
+  }
   .login-card {
+    background: rgba(0,0,0,0.5);
+    z-index: 2;
     width: 440px;
     height: 340px;
     .title{
