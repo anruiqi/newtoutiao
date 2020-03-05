030da3a0a67abec79447f7c4ec1d3553d2d876a8
anruiqi <59909691+anruiqi@users.noreply.github.com>
2020-03-05 22:05:45 +0800

    面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,23 @@
+<template>
+<!-- 使用elementUI的 面包屑  设置分隔符-->
+  <el-breadcrumb separator=">">
+      <!-- el-breadcrumb-item 相当于 router-link标签 -->
+     <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+     <!-- 第二个选项是动态的  需要采用传值的方式传进来 props传值 slot插槽(采用) -->
+     <el-breadcrumb-item>
+         <!-- 内容是动态的 -->
+         <!-- 定义一个具名插槽 -->
+         <slot name='title'></slot>
+     </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
