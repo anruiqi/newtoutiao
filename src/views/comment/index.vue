1250998e797b465d1e19caa87b7fb2a2f8470f4a
anruiqi <59909691+anruiqi@users.noreply.github.com>
2020-03-05 23:12:32 +0800

    评论列表-评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -7,11 +7,14 @@
         </bread-crumb>
         <!-- 表格组件 -->
         <el-table :data="list">
-            <el-table-column width="650px" label="标题"></el-table-column>
-            <el-table-column label="评论状态"></el-table-column>
-            <el-table-column label="总评论数"></el-table-column>
-            <el-table-column label="粉丝评论数"></el-table-column>
-            <el-table-column label="操作"></el-table-column>
+            <el-table-column prop="title" width="600px"  label="标题"></el-table-column>
+            <el-table-column :formatter="formatterBool" prop="comment_status" label="评论状态"></el-table-column>
+            <el-table-column  prop="total_comment_count" label="总评论数"></el-table-column>
+            <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+            <el-table-column  label="操作">
+                <el-button size="small" type='text'>修改</el-button>
+                <el-button size="small" type='text'>关闭评论</el-button>
+            </el-table-column>
         </el-table>
     </el-card>
 </template>
@@ -21,12 +24,36 @@ export default {
   data () {
     return {
       list: [
-        {},
-        {},
-        {},
-        {}
+
       ]
     }
+  },
+  methods: {
+    //   获取评论
+    getComment () {
+      this.$axios({
+        // 请求地址
+        url: '/articles',
+        params: {
+          response_type: 'comment'
+        }
+      }).then(result => {
+        // 返回的结果
+        this.list = result.data.results
+      })
+    },
+    // 格式化函数
+    // row代表当前一行数据
+    // column 代表当前列的信息
+    // cellValue 代表当前单元格的值
+    // index 代表当前的索引
+    formatterBool (row, column, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    // 在钩子函数中获取数据
+    this.getComment()
   }
 }
 </script>
