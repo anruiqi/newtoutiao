e16d00488ab6c85f9d4d2060e6d50b03ecbd95a5
anruiqi <59909691+anruiqi@users.noreply.github.com>
2020-03-05 22:29:02 +0800

    评论列表-页面结构-table

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,10 +1,33 @@
 <template>
-  <div>1564654</div>
+    <el-card>
+        <bread-crumb slot='header'>
+           <template slot="title">
+               评论管理
+           </template>
+        </bread-crumb>
+        <!-- 表格组件 -->
+        <el-table :data="list">
+            <el-table-column width="650px" label="标题"></el-table-column>
+            <el-table-column label="评论状态"></el-table-column>
+            <el-table-column label="总评论数"></el-table-column>
+            <el-table-column label="粉丝评论数"></el-table-column>
+            <el-table-column label="操作"></el-table-column>
+        </el-table>
+    </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: [
+        {},
+        {},
+        {},
+        {}
+      ]
+    }
+  }
 }
 </script>
 
