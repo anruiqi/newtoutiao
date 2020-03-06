e53a677e765d0abca4a634050f566dbb7fbc55fb
anruiqi <59909691+anruiqi@users.noreply.github.com>
2020-03-06 22:48:24 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -6,12 +6,29 @@
              素材管理
          </template>
      </bread-crumb>
+     <!-- 放置标签页 -->
+     <el-tabs v-model="activeName">
+         <!-- 放置标签 label表示标签显示的名称 name 代表页签选中的值-->
+         <el-tab-pane label="全部素材" name='all'>
+             <!-- 内容 -->
+             全部素材
+         </el-tab-pane>
+         <el-tab-pane label="收藏素材" name='collect'>
+             <!-- 内容 -->
+             收藏素材
+         </el-tab-pane>
+     </el-tabs>
  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      // 当前激活的页签 默认选中的素材
+      activeName: 'all'
+    }
+  }
 }
 </script>
 
