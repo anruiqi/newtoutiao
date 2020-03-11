69afb9e2765318c40fa3f3537d5afe1b4e4c4845
anruiqi <59909691+anruiqi@users.noreply.github.com>
2020-03-11 22:26:15 +0800

    页面进度条引入应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,7 +1,13 @@
-import router from '@/router'
 // 注册全局前置守卫
+import router from '@/router'
+// 进度条包
+import progress from 'nprogress'
+// 样式文件
+import 'nprogress/nprogress.css'
 
 router.beforeEach(function (to, from, next) {
+  // 开启进度条
+  progress.start()
   // 判断页面需要权限
   if (to.path.startsWith('/home')) {
     const token = window.localStorage.getItem('user-token')
@@ -13,4 +19,9 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()
   }
+})
+router.afterEach(() => {
+  // 处理 进度条关闭的问题
+  // setTimeout(() => progress.done(), 300)
+  progress.done()
 })
